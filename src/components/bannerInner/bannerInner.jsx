b52543ff9aa0602e1db3b3b4c66952517a80431e
kononiaka <kononiaka.vladimir@gmail.com>
2022-10-20 20:25:22 +0200

    pre-build commit

--- src/components/bannerInner/bannerInner.jsx
+++ src/components/bannerInner/bannerInner.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import ContainerWrapper from './../container_wrapper/container_wrapper';
+import Button from '../UI/Button/Button';
 
 import classes from "./bannerInner.module.css";
-import ContainerWrapper from './../container_wrapper/container_wrapper';
 
 const BannerInner = (props) => {
     return (
@@ -10,9 +11,7 @@ const BannerInner = (props) => {
                 <h2>
                     Start Your Trading Journey with TRADING AS A BUSINESS with a 7 Day Trial!
                 </h2>
-                <div className={classes.banner__btn} onClick={props.handleClick}>
-                    Start Your 7-Day Trial Now!
-                </div>
+                <Button click={props.handleClick}></Button>
             </ContainerWrapper>
         </div>
     );
