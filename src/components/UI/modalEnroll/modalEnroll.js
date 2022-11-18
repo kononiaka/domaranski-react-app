37441d5ec16271789b289bec374f533cb4f528d4
kononiaka <kononiaka.vladimir@gmail.com>
2022-11-18 22:02:08 +0100

    styling was added part 1

--- src/components/UI/modalEnroll/modalEnroll.js
+++ src/components/UI/modalEnroll/modalEnroll.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Modal from '../Modal';
 import cc_logo from '../../../img/credit-card.png';
 import pp_logo from '../../../img/paypal_icon.png';
@@ -12,8 +14,23 @@ const ModalEnroll = (props) => {
         query: '(min-width: 900px)'
     });
 
-    const confirmHandler = () => {
+    const [showCreditBlock, setShowCreditBlock] = useState(false);
+    const [showPayPalBlock, setShowPayPalBlock] = useState(false);
+
+    const confirmHandler = (e) => {
+        e.preventDefault();
         console.log('logged');
+    };
+
+    const showCreditCard = (e) => {
+        e.preventDefault();
+        setShowCreditBlock(true);
+        setShowPayPalBlock(false);
+    };
+    const showPayPal = (e) => {
+        e.preventDefault();
+        setShowPayPalBlock(true);
+        setShowCreditBlock(false);
     };
 
     return (
@@ -30,33 +47,33 @@ const ModalEnroll = (props) => {
                 <div className={classes.form_group}>
                     <div className={classes.form_control}>
                         <label htmlFor="email" >Email</label>
-                        <input name="email" type="text" placeholder="Email" data-vv-id="1" aria-required="true" aria-invalid="false" />
+                        <input name="email" type="text" className="form-control input" placeholder="Email" data-vv-id="1" aria-required="true" aria-invalid="false" />
                     </div>
                     <div className={classes.form_control}>
                         <label htmlFor="First Name">First Name</label>
-                        <input name="first_name" type="text" placeholder="Email" data-vv-id="1" aria-required="true" aria-invalid="false"></input>
+                        <input name="first_name" type="text" className="form-control input" placeholder="First Name" data-vv-id="1" aria-required="true" aria-invalid="false"></input>
                     </div>
                     <div className={classes.form_control}>
                         <label htmlFor="Last Name">Last Name</label>
-                        <input name="last_name" type="text" placeholder="Email" data-vv-id="1" aria-required="true" aria-invalid="false"></input>
+                        <input name="last_name" type="text" className="form-control input" placeholder="Last Name" data-vv-id="1" aria-required="true" aria-invalid="false"></input>
                     </div>
                     <div className={classes.pay_control}>
-                        <div className={classes.pay_btn}>
-                            <img src={cc_logo} width="32" alt="cc_logo" /><button>Credit Card</button>
+                        <div className={classes.pay_btn} >
+                            <img src={cc_logo} width="32" alt="cc_logo" /><button onClick={showCreditCard}>Credit Card</button>
                         </div>
                         <div className={classes.pay_btn}>
-                            <img src={pp_logo} width="32" alt="pp_logo" /><button>Paypal</button>
+                            <img src={pp_logo} width="32" alt="pp_logo" /><button onClick={showPayPal}>Paypal</button>
                         </div>
                     </div>
 
-                    <div>
-                        <div>
+                    {showCreditBlock && <div className={classes.credit_card_block}>
+                        <div className={classes.credit_card_block_labels}>
                             <label htmlFor="creditcard">Credit Card</label>
-                            <div>
-                                <input data-vv-as="Credit Card" type="text" id="creditcard" name="creditcard" placeholder="Credit Card #" className="form-control input" data-vv-id="14" aria-required="true" aria-invalid="false" /></div>
-                            <span></span>
-                        </div> <div ><label htmlFor="month">Expiration Date</label>
-                            <div><div>
+                            <label htmlFor="month">Expiration Date</label>
+                        </div>
+                        <div className={classes.credit_card_block_labels}>
+                            <input data-vv-as="Credit Card" type="text" id="creditcard" name="creditcard" placeholder="Credit Card #" className="form-control input" data-vv-id="14" aria-required="true" aria-invalid="false" />
+                            <div className={classes.credit_card_block_checkbox}>
                                 <select id="month" name="month" data-vv-id="15" aria-required="true" aria-invalid="false">
                                     <option value="">Month</option>
                                     <option value="01">01</option>
@@ -72,22 +89,58 @@ const ModalEnroll = (props) => {
                                     <option value="11">11</option>
                                     <option value="12">12</option>
                                 </select>
-                                <select id="year" name="year" className="form-control col-sm-6" data-vv-id="16" aria-required="true" aria-invalid="false"><option value="">Year</option> <option value="2022">2022</option><option value="2023">2023</option><option value="2024">2024</option><option value="2025">2025</option><option value="2026">2026</option><option value="2027">2027</option><option value="2028">2028</option><option value="2029">2029</option><option value="2030">2030</option><option value="2031">2031</option><option value="2032">2032</option><option value="2033">2033</option><option value="2034">2034</option><option value="2035">2035</option><option value="2036">2036</option><option value="2037">2037</option><option value="2038">2038</option><option value="2039">2039</option><option value="2040">2040</option><option value="2041">2041</option><option value="2042">2042</option></select></div></div> <span className="help is-danger col-sm-12" ></span> <span className="help is-danger col-sm-12">
-                            </span>
+                                <select id="year" name="year" data-vv-id="16" aria-required="true" aria-invalid="false">
+                                    <option value="">Year</option>
+                                    <option value="2022">2022</option>
+                                    <option value="2023">2023</option>
+                                    <option value="2024">2024</option>
+                                    <option value="2025">2025</option>
+                                    <option value="2026">2026</option>
+                                    <option value="2027">2027</option>
+                                    <option value="2028">2028</option>
+                                    <option value="2029">2029</option>
+                                    <option value="2030">2030</option>
+                                    <option value="2031">2031</option>
+                                    <option value="2032">2032</option>
+                                    <option value="2033">2033</option>
+                                    <option value="2034">2034</option>
+                                    <option value="2035">2035</option>
+                                    <option value="2036">2036</option>
+                                    <option value="2037">2037</option>
+                                    <option value="2038">2038</option>
+                                    <option value="2039">2039</option>
+                                    <option value="2040">2040</option>
+                                    <option value="2041">2041</option>
+                                    <option value="2042">2042</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div >
-                        <ul ><li><label>
-                            <input type="radio" name="paypalYear" value="1" /> <span>1 year</span> <span>$499</span>
-                        </label></li> <li><label><input type="radio" name="paypalYear" value="2" /> <span>2 years</span>
-                            <span> $948</span> <span className="region_desc"> $50 discount applied</span></label></li>
-                            <li><label><input type="radio" name="paypalYear" value="3" /> <span>3 years</span>
-                                <span> $1397</span> <span className="region_desc"> $100 discount applied</span></label></li>
-                        </ul></div>
+                    </div>}
+
+                    {showPayPalBlock && <div className={classes.paypal_block}>
+                        <ul style={{ listStyle: 'none' }}>
+                            <li>
+                                <label>
+                                    <input type="radio" name="paypalYear" value="1" />
+                                    <span> 1 year</span> <span>$999</span>
+                                </label>
+                            </li>
+                            <li>
+                                <label>
+                                    <input type="radio" name="paypalYear" value="2" />
+                                    <span> 2 years</span> <span> $1899</span>
+                                    <span>$100 discount applied</span>
+                                </label>
+                            </li>
+                        </ul>
+                    </div>}
                 </div>
-                <button type="submit">Start My Free Trial</button>
-                <div>
-                    or <a href="">Subscribe Now</a></div>
+                <div className={classes.button_block}>
+                    <button type="submit" onClick={confirmHandler} className={classes.submitBtn}>Start My Free Trial</button>
+                    <div>
+                        or <a href="">Subscribe Now</a>
+                    </div>
+                </div>
             </form>
         </Modal>
     );
